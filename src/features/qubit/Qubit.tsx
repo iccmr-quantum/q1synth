ca6e37af710c341aa3e369ab3d548bb0b02b9867
petethomasmusique <petethomasmusique@gmail.com>
2022-02-18 09:11:16 +0000

    state labels

--- src/features/qubit/Qubit.tsx
+++ src/features/qubit/Qubit.tsx
@@ -49,6 +49,15 @@ export function Qubit() {
 
     const qubitRef = useRef<null | HTMLDivElement>(null)
 
+    const states = [
+        {id: '0', label: '0'},
+        {id: '1', label: '1'},
+        {id: 'plus', label: '+'},
+        {id: 'minus', label: '-'},
+        {id: 'i', label: 'i'},
+        {id: 'minusi', label: '-i'},
+    ]
+
     const handleMouseMove = (e: MouseEvent) => {
         const rect = qubitRef.current?.getBoundingClientRect()
         const left = rect?.left || 0
@@ -72,6 +81,8 @@ export function Qubit() {
             onMouseLeave={() => setIsClicked(false)}
             onMouseMove={(e) => isClicked && handleMouseMove(e)}
         >
+            {states.map(({id, label}) => <span className={`${styles.label} ${styles['label' + id]}`}>{`|${label}>`}</span>)}
+            
             <ReactP5Wrapper 
                 sketch={sketch} 
                 x={x.value * 360}
