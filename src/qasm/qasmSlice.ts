783ba8d52c9ade74f8d51f6ce5b93685516b303c
petethomasmusique <petethomasmusique@gmail.com>
2022-03-16 16:18:19 +0000

    socket: set up qasmslice

--- src/qasm/qasmSlice.ts
+++ src/qasm/qasmSlice.ts
@@ -2,13 +2,19 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Dictionary } from '../types';
 import { RootState } from '../app/store';
 
+export interface QasmResponse {
+    id: number
+    data: any
+}
 
 export interface QasmState extends Dictionary {
     qasmStatus: boolean
+    responses: QasmResponse[]
 }
 
 const initialState: QasmState = {
     qasmStatus: false,
+    responses: []
 };
 
 export const qasmSlice = createSlice({
@@ -17,15 +23,25 @@ export const qasmSlice = createSlice({
     reducers: {
         setQasmStatus: (state, action: PayloadAction<boolean>) => {
             state.qasmStatus = action.payload
+        },
+        setQasmResponse: (state, action: PayloadAction<QasmResponse>) => {
+            state.responses = [
+                ...state.responses,
+                action.payload
+            ]
         }
     }
 });
 
 export const { 
-    setQasmStatus
+    setQasmStatus,
+    setQasmResponse
 } = qasmSlice.actions;
 
-export const getQasmStatus = (state: RootState) => state.data.qasmStatus;
+export const getQasmStatus = (state: RootState) => state.qasm.qasmStatus;
+export const getQasmResponseByID = (i: number) => (state: RootState) => {
+    return state.qasm.responses.find(({id}) => id === i) || false
+};
 
 
 export default qasmSlice.reducer;
