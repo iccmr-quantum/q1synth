cfd8040e0940bffc916214b7e9c128f95127566c
petethomasmusique <petethomasmusique@gmail.com>
2022-03-24 12:22:45 +0000

    socket: loading screen...

--- src/features/loading/Loading.tsx
+++ src/features/loading/Loading.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
 import styles from './Loading.module.css'
+
 
 export function Loading() {
     return (
         <div className={styles.container}>
-            
+            <div className={styles.overlay} />
+            <FontAwesomeIcon icon={faCircleNotch} className={styles.spinner} />
+            <p className={styles.copy}>Talking to quantum computer<span className={styles.cursor}>_</span></p>
         </div>
     )
 }
