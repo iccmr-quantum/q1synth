f70ff5b12e418b182d33c415a5f87a10bacdba6d
petethomasmusique <petethomasmusique@gmail.com>
2022-09-01 17:02:28 +0100

    set up loop

--- src/sound/index.ts
+++ src/sound/index.ts
@@ -29,7 +29,7 @@ async function startAudio() : Promise<void> {
     console.log('starting audio')
     await Tone.start()
     console.log('audio is running')
-    Tone.setContext(new Tone.Context({ latencyHint: 'playback' }))
+    // Tone.setContext(new Tone.Context({ latencyHint: 'playback' }))
     window.removeEventListener('click', startAudio)
 }
 
--- src/synthesis/synthesis.js
+++ src/synthesis/synthesis.js
@@ -1,35 +1,41 @@
-import { Loop, Transport } from "tone";
-import { synth } from "../sound";
-import { SynthType } from "./synthesisSlice";
+import { Loop, Transport, getDestination, now, immediate } from "tone";
+import { CtFMSynth, CtGranular, CtDualSynth } from "./ct-synths"; // TODO: replace this with node_module
 
 const synthesis = () => {
-    let params = {};
+    let synth
+    let params = {n: 48, dur: 1, amp: 0.5}
     let synthType = 'fm'
     let duration = 1;
-    let count = 0
+    let count = -1
     
-    new Loop((time) => {
+    Transport.scheduleRepeat((time) => {
+        count++;
         if(count%Math.floor(48/duration)) return
         switch(synthType) {
             case 'fm':
-                // fm(time)
+                synth = new CtFMSynth()
                 break
             case 'granular':
-                // am(time)
+                synth = new CtGranular()
                 break
             case 'subtractive':
-                // noise(time)
+                synth = new CtDualSynth()
                 break
             default:
-                // fm
+                synth = new CtFMSynth()
                 break
         }
-        count++;
-    }, "48n").start(0);
+        // TODO: connect to reverb and delay instead
+        synth.connect(getDestination())
+        synth.play(params, time)
+    }, "48n");
 
     return {
         play: () => Transport.start(),
-        stop: () => Transport.stop(),
+        stop: () => {
+            count = -1
+            Transport.stop()
+        },
         setParams: (ps) => {
             params = ps;
             synth && synth.mutate(params);
