19c8bf80e2f6646fc693435520ddde144bd98ef1
petethomasmusique <petethomasmusique@gmail.com>
2022-09-17 20:56:45 +0100

    copy across socket to udp script from zen

---
+++ src/qasm/socket-to-udp.js
@@ -0,0 +1,29 @@
+// TODO
+const WebSocket = require("ws");
+const osc = require("osc");
+
+const udpPort = new osc.UDPPort({
+    localAddress: "127.0.0.1",
+    localPort: 57121,
+    // This is where sclang is listening for OSC messages.
+    remoteAddress: "127.0.0.1",
+    remotePort: 57120,
+    metadata: true
+});
+
+udpPort.open();
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", (socket) => {
+    console.log("A Web Socket connection has been established!");
+    const socketPort = new osc.WebSocketPort({
+        socket: socket,
+        metadata: true
+    });
+
+    // act as a relay, forwarding all websocket message on via udp - for SC
+    new osc.Relay(socketPort, udpPort, {
+        raw: true
+    });
+});
