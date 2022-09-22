ce659f09571515e050c16d1dc9398ddc74526414
petethomasmusique <petethomasmusique@gmail.com>
2022-09-22 12:04:14 +0100

    add new samples, revert ct-synths changes

--- src/synthesis/synthesis.js
+++ src/synthesis/synthesis.js
@@ -5,13 +5,15 @@ import { beatsToSeconds, formatMutationParams } from "./utils";
 const limiter = new Limiter(-1).toDestination()
 const reverb = new Reverb(4).connect(limiter)
 const delay = new FeedbackDelay('4n', 0.5).connect(reverb)
+const origin = window.location.origin
 const urls = [
     'https://tonejs.github.io/audio/berklee/arpeggio3crazy.mp3',
     'https://tonejs.github.io/audio/berklee/taps_1c.mp3',
     'https://tonejs.github.io/audio/berklee/tinkle3.mp3',
     'https://tonejs.github.io/audio/berklee/tapping1.mp3',
     'https://tonejs.github.io/audio/berklee/gong_1.mp3',
-    'https://tonejs.github.io/audio/berklee/gurgling_theremin_1.mp3'
+    'https://tonejs.github.io/audio/berklee/gurgling_theremin_1.mp3',
+    origin + '/samples/IBM_Refrigerators.wav',
 ]
 const samples = urls.map(url => url.substring(url.lastIndexOf('/') + 1))
 const buffers = urls.map(url => new ToneAudioBuffer(url))
